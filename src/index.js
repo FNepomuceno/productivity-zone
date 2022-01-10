bbfccd71dd630f181b050a302cb3268d47d24ef7
FNepomuceno <francisnepo@gmail.com>
2022-01-10 09:51:53 -0500

    Added user state

--- src/index.js
+++ src/index.js
@@ -11,8 +11,28 @@ function TitleDisplay(props) {
 
 class ProfileInfo extends React.Component {
 	render() {
+		let user = this.props.user;
+
+		if (user) {
+			return (
+				<div className="profile-info">
+					<p>Welcome, user.</p>
+					<p>
+						<span class="login-text" onClick={this.props.logout}>Logout</span>
+						&nbsp;to switch profiles
+					</p>
+				</div>
+			);
+		}
+
 		return (
-			<p className="profile-info">Welcome, guest.</p>
+			<div className="profile-info">
+				<p>Welcome, guest.</p>
+				<p>
+					<span class="login-text" onClick={this.props.login}>Login</span>
+					&nbsp;to save stats
+				</p>
+			</div>
 		);
 	}
 }
@@ -22,17 +42,34 @@ class Header extends React.Component {
 		return (
 			<div className="header">
 				<TitleDisplay />
-				<ProfileInfo />
+				<ProfileInfo
+					user={this.props.user}
+					login={this.props.login}
+					logout={this.props.logout}
+				/>
 			</div>
 		);
 	}
 }
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleLoginClick = this.handleLoginClick.bind(this);
+		this.handleLogoutClick = this.handleLogoutClick.bind(this);
+		this.state = {
+			user: null,
+		};
+	}
+
 	render() {
 		return (
 			<div className="app-container">
-				<Header />
+				<Header
+					user={this.state.user}
+					login={this.handleLoginClick}
+					logout={this.handleLogoutClick}
+				/>
 				<div className="container">
 					<div className="col-filler" />
 					<div className="content">
@@ -45,6 +82,23 @@ class App extends React.Component {
 				</div>
 			</div>
 		);
+	}
+
+	handleLoginClick() {
+		// TODO: modal for selecting profile
+		this.setState({
+			user: {
+				name: "Productivity Boss",
+			},
+		});
+		console.log("login?");
+	}
+
+	handleLogoutClick() {
+		// TODO: modal for logging out
+		this.setState({
+			user: null,
+		});
 	}
 }
 
