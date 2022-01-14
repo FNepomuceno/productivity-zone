083753eca35e966fd8c027908ab6200a3a95fe13
FNepomuceno <francisnepo@gmail.com>
2022-01-14 10:20:47 -0500

    Extracted ContentContainer component into separate file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import Header from './Header.js';
-import TasksApp from './TasksApp.js';
+import ContentContainer from './ContentContainer.js';
 
 import {
 	startDB,
@@ -11,67 +11,6 @@ import {
 } from './database.js';
 
 // --- Components ---
-class ContentContainer extends React.Component {
-	constructor(props) {
-		super(props);
-		this.default = <p>Choose an app above</p>;
-	}
-
-	render() {
-		let typePairs = [
-			["Tasks", <TasksApp db={this.props.db} user={this.props.user} />],
-			["Timer", <p>Timer App under construction</p>],
-		];
-
-		let types = typePairs.reduce((lst, entry) => {
-			lst.push(entry[0]);
-			return lst;
-		}, []);
-
-		let apps = typePairs.reduce((lst, entry) => {
-			lst.push(entry[1]);
-			return lst;
-		}, []);
-
-		let type;
-		let content = this.default;
-
-		for (let i in types) {
-			if (types[i] === this.props.type) {
-				type = types[i];
-				content = apps[i];
-				break;
-			}
-		}
-
-		let options = types.map((optType) => {
-			let classes = "tab-option noselect";
-			if (optType === type) { classes = classes + " tab-selected"; }
-
-			let handleClick = this.props.switchTab(optType);
-
-			return (
-				<div className={classes} key={optType} onClick={handleClick}>
-					<h2>{optType}</h2>
-				</div>
-			);
-		});
-
-		return (
-			<div className="container">
-				<div className="col-filler" />
-				<div className="content">
-					<div className="tab-selector">
-						{options}
-					</div>
-					{content}
-				</div>
-				<div className="col-filler" />
-			</div>
-		);
-	}
-}
-
 class App extends React.Component {
 	constructor(props) {
 		super(props);
