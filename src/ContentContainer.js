20d03bb1f1652a5de19f1fa4130d6ce14ac0a5f7
FNepomuceno <francisnepo@gmail.com>
2022-01-24 11:11:22 -0500

    Convert ContentContainer component into function

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -1,22 +1,15 @@
-import React from 'react';
 import './ContentContainer.css';
 
-class ContentContainer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="col-filler" />
-                <div className="content">
-                    {this.props.children}
-                </div>
-                <div className="col-filler" />
+function ContentContainer(props) {
+    return (
+        <div className="container">
+            <div className="col-filler" />
+            <div className="content">
+                {props.children}
             </div>
-        );
-    }
+            <div className="col-filler" />
+        </div>
+    );
 }
 
 export default ContentContainer;
